a515826898f4f5f9d2dda641c046301da732a130
Ian <ifd730@gmail.com>
2023-05-05 12:29:06 -0700

    CORS Changes

--- app.js
+++ app.js
@@ -30,12 +30,6 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  next();
-});
-
 app.use(limiter);
 
 app.use(helmet());
