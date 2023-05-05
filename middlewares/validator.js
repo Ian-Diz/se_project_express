07532dbb115958808b82dd8cf196da0a6328e34e
Ian <ifd730@gmail.com>
2023-05-05 13:22:46 -0700

    Fixed avatar being required

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -42,7 +42,7 @@ module.exports.validateUserInfo = () => {
         "string.max": 'The maximum length of the "name" field is 30',
         "string.empty": 'The "name" field must be filled in',
       }),
-      avatar: Joi.string().required().custom(validateUrl).messages({
+      avatar: Joi.string().custom(validateUrl).messages({
         "string.empty": 'The "avatar" field must be filled in',
         "string.uri": 'The "avatar" field must be a valid url',
       }),
